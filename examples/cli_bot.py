487fff1ad46a057aa749d4ab1fb7a87ae6d930a5
decorator-factory <42166884+decorator-factory@users.noreply.github.com>
2022-01-07 02:32:17 +0300

    Add CLI bot example

---
+++ examples/cli_bot.py
@@ -0,0 +1,33 @@
+from bot_command_parser.converters import seq, word, integer, literal, ParseError
+
+
+quit = literal("/quit")
+repeat = seq + literal("/repeat") + word + integer
+
+
+
+print("Commands:")
+print("    /quit")
+print("    /repeat <word> <times>")
+
+while True:
+    try:
+        command = input(">>> ")
+    except (KeyboardInterrupt, EOFError):
+        print("\nBye!")
+        break
+
+    if not command:
+        continue
+
+    if quit.matches(command):
+        print("Bye!")
+        break
+
+    try:
+        _, (_, phrase, times) = repeat.parse(command)
+    except ParseError as exc:
+        print(exc.describe())
+    else:
+        for _ in range(times):
+            print(phrase)
